22911ffdc85d22dae7428269ef32cf6e8c464376
Jens Bendisposto <jens@bendisposto.de>
2010-08-23 18:10:02 +0200

    Starting with Application

---
+++ gamelib/world.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# 
+import pyknic
+import pyknic.world
+import pygame
+from pygame.locals import *
+
+import pyknic.resources.tiledtmxloader
+
+
+class PlayState(pyknic.State):
+    pass
