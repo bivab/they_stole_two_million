2a7fc1dd713e383618daab562326390adf99cca4
Jens Bendisposto <jens@bendisposto.de>
2010-08-23 18:17:10 +0200

    world + state

--- gamelib/world.py
+++ gamelib/world.py
@@ -9,4 +9,15 @@ import pyknic.resources.tiledtmxloader
 
 
 class PlayState(pyknic.State):
+    def __init__(self,  *args, **kwargs):
+        super(PlayState, self).__init__(*args, **kwargs)
+        self.world = TheWorld()
+
+        
+    def on_init(self, app):
+        pass
+        
+        
+        
+class TheWorld(pyknic.world.IWorld):
     pass
