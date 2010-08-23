9624dd68398d16f60132ace6d3418ac4639280b5
Jens Bendisposto <jens@bendisposto.de>
2010-08-23 18:40:09 +0200

    frickel frickel

--- gamelib/world.py
+++ gamelib/world.py
@@ -1,11 +1,28 @@
 # -*- coding: utf-8 -*-
 # 
 import pyknic
+import pyknic.timing
 import pyknic.world
+import pyknic.utilities
+import pyknic.animation
+
+from pyknic.utilities import SortedList
+from pyknic.timing import GameTime
+from pyknic.geometry import Vec3
+from pyknic.entity import Entity
+from pyknic.entity import Spr
+from pyknic import renderer
+from pyknic import gui
+
+
 import pygame
+from pygame.sprite import DirtySprite
 from pygame.locals import *
 
-import pyknic.resources.tiledtmxloader
+import random
+
+from pyknic.resources.tiledtmxloader import TileMapParser, ImageLoaderPygame
+
 
 
 class PlayState(pyknic.State):
@@ -15,9 +32,37 @@ class PlayState(pyknic.State):
 
         
     def on_init(self, app):
-        pass
-        
+        world_map = TileMapParser().parse_decode("data/testtile.tmx")
+        world_map.load(ImageLoaderPygame())
+        assert world_map.orientation == "orthogonal"
+        for layernum, layer in enumerate(world_map.layers[:]):
+            if layer.visible:
+                layer_img = pygame.Surface((layer.pixel_width, layer.pixel_height))
+                layer_img.fill((255, 0, 255))
+                layer_img.set_colorkey((255, 0, 255))
+                idx = 0
+                for y in xrange(0, layer.pixel_height, world_map.tileheight):
+                    for x in xrange(0, layer.pixel_width, world_map.tilewidth):
+                        img_idx = layer.decoded_content[idx]
+                        idx += 1
+                        if img_idx:
+                            offx, offy, screen_img = world_map.indexed_tiles[img_idx]
+                            screen_img = screen_img.convert()
+                            if layer.opacity > -1:
+                                screen_img.set_alpha(None)
+                                alpha_value = int(255. * float(layer.opacity))
+                                screen_img.set_alpha(alpha_value)
+                            layer_img.blit(screen_img.convert(), (x + offx, y + offy))
+                print layer.name, layer.opacity
+                layer_img.set_alpha(int(255. * float(abs(layer.opacity))))
+                spr = Spr()
+                spr.image = layer_img.convert_alpha()
+                ent = Entity(spr, Vec3(0, 0))
+                ent.rect.size = spr.image.get_size()
+                ent.layer = layernum * 10
+                self.world.add_entity(ent)
         
         
 class TheWorld(pyknic.world.IWorld):
-    pass
+    def add_entity(self,entity):
+        pass
